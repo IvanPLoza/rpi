25a05f99ef9b0ca48942b2f7909b0c39ea48c432
Ivan Pavao Lozancic <ivano.lozani@gmail.com>
2020-01-17 23:18:05 +0100

    Fixed motor controll

--- src/gpioControlls/index.js
+++ src/gpioControlls/index.js
@@ -2,44 +2,53 @@ var rpio = require('rpio');
 
 const MOTOR_HL1 = 12; // Left motor forward
 const MOTOR_HL2 = 13; // Left motor backward
-const MOTOR_HR1 = 21; // Right motor backward
+const MOTOR_HR1 = 20; // Right motor backward
 const MOTOR_HR2 = 21; // right motor forward
+const MOTOR_LEFT_EN = 6; // Enable left motor 
+const MOTOR_RIGHT_EN = 26; // Enable right motor
+
+rpio.open(MOTOR_HL1, rpio.OUTPUT, rpio.LOW);
+rpio.open(MOTOR_HL2, rpio.OUTPUT, rpio.LOW);
+rpio.open(MOTOR_HR1, rpio.OUTPUT, rpio.LOW);
+rpio.open(MOTOR_HR2, rpio.OUTPUT, rpio.LOW);
+rpio.open(MOTOR_LEFT_EN, rpio.OUTPUT, rpio.LOW);
+rpio.open(MOTOR_LEFT_EN, rpio.OUTPUT, rpio.LOW);
 
 class motorControll{
 
     static stopAll(){
-        rpio.open(MOTOR_HL1, rpio.OUTPUT, rpio.LOW);
-        rpio.open(MOTOR_HL2, rpio.OUTPUT, rpio.LOW);
-        rpio.open(MOTOR_HR1, rpio.OUTPUT, rpio.LOW);
-        rpio.open(MOTOR_HR2, rpio.OUTPUT, rpio.LOW);
+        rpio.write(MOTOR_HL1, rpio.LOW);
+        rpio.write(MOTOR_HL2, rpio.LOW);
+        rpio.write(MOTOR_HR1, rpio.LOW);
+        rpio.write(MOTOR_HR2, rpio.LOW);
     }
 
     static goBackwards(){
-        rpio.open(MOTOR_HL1, rpio.OUTPUT, rpio.LOW);
-        rpio.open(MOTOR_HL2, rpio.OUTPUT, rpio.HIGH);
-        rpio.open(MOTOR_HR1, rpio.OUTPUT, rpio.HIGH);
-        rpio.open(MOTOR_HR2, rpio.OUTPUT, rpio.LOW);
+        rpio.write(MOTOR_HL1, rpio.LOW);
+        rpio.write(MOTOR_HL2, rpio.HIGH);
+        rpio.write(MOTOR_HR1, rpio.HIGH);
+        rpio.write(MOTOR_HR2, rpio.LOW);
     }
 
     static goForward(){
-        rpio.open(MOTOR_HL1, rpio.OUTPUT, rpio.HIGH);
-        rpio.open(MOTOR_HL2, rpio.OUTPUT, rpio.LOW);
-        rpio.open(MOTOR_HR1, rpio.OUTPUT, rpio.LOW);
-        rpio.open(MOTOR_HR2, rpio.OUTPUT, rpio.HIGH);
+        rpio.write(MOTOR_HL1, rpio.HIGH);
+        rpio.write(MOTOR_HL2, rpio.LOW);
+        rpio.write(MOTOR_HR1, rpio.LOW);
+        rpio.write(MOTOR_HR2, rpio.HIGH);
     }
 
     static goRight(){
-        rpio.open(MOTOR_HL1, rpio.OUTPUT, rpio.HIGH);
-        rpio.open(MOTOR_HL2, rpio.OUTPUT, rpio.LOW);
-        rpio.open(MOTOR_HR1, rpio.OUTPUT, rpio.LOW);
-        rpio.open(MOTOR_HR2, rpio.OUTPUT, rpio.LOW);
+        rpio.write(MOTOR_HL1, rpio.HIGH);
+        rpio.write(MOTOR_HL2, rpio.LOW);
+        rpio.write(MOTOR_HR1, rpio.LOW);
+        rpio.write(MOTOR_HR2, rpio.LOW);
     }
 
     static goLeft(){
-        rpio.open(MOTOR_HL1, rpio.OUTPUT, rpio.LOW);
-        rpio.open(MOTOR_HL2, rpio.OUTPUT, rpio.LOW);
-        rpio.open(MOTOR_HR1, rpio.OUTPUT, rpio.LOW);
-        rpio.open(MOTOR_HR2, rpio.OUTPUT, rpio.HIGH);
+        rpio.write(MOTOR_HL1, rpio.LOW);
+        rpio.write(MOTOR_HL2, rpio.LOW);
+        rpio.write(MOTOR_HR1, rpio.LOW);
+        rpio.write(MOTOR_HR2, rpio.HIGH);
     }
 }
 
