a6acd3419e3b8fc3c3398c33ba9d41e3a558ae2d
Ivan Pavao Lozancic <ivano.lozani@gmail.com>
2020-01-17 22:25:36 +0100

    Project init. RPIO lib addded, rpi pi camera lib added, motor control test written

---
+++ app.js
@@ -0,0 +1,2 @@
+
+testMotors();
---
+++ src/gpioControlls/index.js
@@ -0,0 +1,55 @@
+var rpio = require('rpio');
+
+const MOTOR_HL1 = 12; // Left motor forward
+const MOTOR_HL2 = 13; // Left motor backward
+const MOTOR_HR1 = 20; // Right motor backward
+const MOTOR_HR2 = 21; // right motor forward
+
+class motorControll{
+
+    static stopAll(){
+        rpio.open(MOTOR_HL1, rpio.OUTPUT, rpio.LOW);
+        rpio.open(MOTOR_HL2, rpio.OUTPUT, rpio.LOW);
+        rpio.open(MOTOR_HR1, rpio.OUTPUT, rpio.LOW);
+        rpio.open(MOTOR_HR2, rpio.OUTPUT, rpio.LOW);
+    }
+
+    static goBackwards(){
+        rpio.open(MOTOR_HL1, rpio.OUTPUT, rpio.LOW);
+        rpio.open(MOTOR_HL2, rpio.OUTPUT, rpio.HIGH);
+        rpio.open(MOTOR_HR1, rpio.OUTPUT, rpio.HIGH);
+        rpio.open(MOTOR_HR2, rpio.OUTPUT, rpio.LOW);
+    }
+
+    static goForward(){
+        rpio.open(MOTOR_HL1, rpio.OUTPUT, rpio.HIGH);
+        rpio.open(MOTOR_HL2, rpio.OUTPUT, rpio.LOW);
+        rpio.open(MOTOR_HR1, rpio.OUTPUT, rpio.LOW);
+        rpio.open(MOTOR_HR2, rpio.OUTPUT, rpio.HIGH);
+    }
+
+    static goRight(){
+        rpio.open(MOTOR_HL1, rpio.OUTPUT, rpio.HIGH);
+        rpio.open(MOTOR_HL2, rpio.OUTPUT, rpio.LOW);
+        rpio.open(MOTOR_HR1, rpio.OUTPUT, rpio.LOW);
+        rpio.open(MOTOR_HR2, rpio.OUTPUT, rpio.LOW);
+    }
+
+    static goLeft(){
+        rpio.open(MOTOR_HL1, rpio.OUTPUT, rpio.LOW);
+        rpio.open(MOTOR_HL2, rpio.OUTPUT, rpio.LOW);
+        rpio.open(MOTOR_HR1, rpio.OUTPUT, rpio.LOW);
+        rpio.open(MOTOR_HR2, rpio.OUTPUT, rpio.HIGH);
+    }
+}
+
+const motor = new motorControll();
+
+function testMotors(){
+    motor.stopAll();
+    setTimeout(motor.goBackwards(), 1000);
+    setTimeout(motor.goForward(), 1000);
+    setTimeout(motor.goLeft(), 1000);
+    setTimeout(motor.goRight(), 1000);
+    setTimeout(motor.stopAll(), 1000);
+}
