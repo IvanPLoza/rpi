6f58d77bda3a3c2212464fc6fb61fdadd2402ca2
jmelvan <jakov.melvan@gmail.com>
2020-01-18 09:30:28 +0100

    ads

--- src/camera/index.js
+++ src/camera/index.js
@@ -6,7 +6,7 @@ var fs = require("fs");
 async function readQR(data){
     const img = await new Promise((resolve) => {
         Jimp.read(data).then((image) => {
-            image.grayscale();
+            image.grayscale().contrast(1).posterize(2);
             image.write("test.jpeg");
             resolve(image);
         });
