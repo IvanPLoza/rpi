33a8378e6fd2aa652e8bcfe6f779a885f046b07b
jmelvan <jakov.melvan@gmail.com>
2020-01-18 11:45:43 +0100

    testing controlls

--- src/gpioControlls/index.js
+++ src/gpioControlls/index.js
@@ -1,9 +1,9 @@
 var rpio = require('rpio');
 
 const MOTOR_HL1 = 32; // Left motor forward
-const MOTOR_HL2 = 33; // Left motor backward
+const MOTOR_HL2 = 40; // Left motor backward
 const MOTOR_HR1 = 38; // Right motor backward
-const MOTOR_HR2 = 40; // right motor forward
+const MOTOR_HR2 = 33; // right motor forward
 const MOTOR_LEFT_EN = 31; // Enable left motor
 const MOTOR_RIGHT_EN = 37; // Enable right motor
 
@@ -17,15 +17,15 @@ var options = {
 rpio.init(options);
 
 rpio.open(MOTOR_HL1, rpio.PWM);
-rpio.open(MOTOR_HL2, rpio.PWM);
-rpio.open(MOTOR_HR1, rpio.PWM);
+rpio.open(MOTOR_HL2, rpio.OUTPUT, rpio.LOW);
+rpio.open(MOTOR_HR1, rpio.OUTPUT, rpio.LOW);
 rpio.open(MOTOR_HR2, rpio.PWM);
 rpio.open(MOTOR_LEFT_EN, rpio.OUTPUT, rpio.HIGH);
 rpio.open(MOTOR_RIGHT_EN, rpio.OUTPUT, rpio.HIGH);
 
 rpio.pwmSetClockDivider(64);
 rpio.pwmSetRange(MOTOR_HL1, 1024);
-//rpio.pwmSetRange(MOTOR_HR2, 1024);
+rpio.pwmSetRange(MOTOR_HR2, 1024);
 
 class motorControll{
 
@@ -33,7 +33,7 @@ class motorControll{
         rpio.pwmSetData(MOTOR_HL1, 0);
         rpio.write(MOTOR_HL2, rpio.LOW);
         rpio.write(MOTOR_HR1, rpio.LOW);
-        rpio.write(MOTOR_HR2, rpio.LOW);
+        rpio.pwmSetData(MOTOR_HR2, 0);
     }
 
     static goBackwards(){
@@ -47,7 +47,7 @@ class motorControll{
         rpio.pwmSetData(MOTOR_HL1, speed);
         rpio.write(MOTOR_HL2, 0);
         rpio.write(MOTOR_HR1, 0);
-        rpio.write(MOTOR_HR2, 0);
+        rpio.pwmSetData(MOTOR_HR2, speed);
     }
 
     static goRight(){
