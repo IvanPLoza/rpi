1ff823f7c83a901fb02a67a5155eed362e784639
jmelvan <jakov.melvan@gmail.com>
2020-01-18 07:23:13 +0100

    asd

--- src/camera/index.js
+++ src/camera/index.js
@@ -6,16 +6,10 @@ var fs = require("fs");
 raspberryPiCamera.on('frame', (data) => {
     console.log(data);
     Jimp.read(data, function(err, img){
-        if (err) {
-            console.error(err);
-            // TODO handle error
-        }
+       
         var qr = new QrCode();
         qr.callback = function(err, value) {
-            if (err) {
-                console.error(err);
-                // TODO handle error
-            }
+            
             console.log(value.result);
             console.log(value);
         };
