bf2b56107501df4bd2fdfff90112382c75bb0a22
jmelvan <jakov.melvan@gmail.com>
2020-01-18 03:02:35 +0100

    fix14

--- src/gpioControlls/index.js
+++ src/gpioControlls/index.js
@@ -7,8 +7,8 @@ const MOTOR_HR1 = 38; // Right motor backward
 const MOTOR_HR2 = 40; // right motor forward
 const MOTOR_LEFT_EN = 31; // Enable left motor
 const MOTOR_RIGHT_EN = 37; // Enable right motor
-const SERVO_1 = 33; // Enable left motor
-const SERVO_2 = 32; // Enable right motor
+const SERVO_1 = 12; // Enable left motor
+const SERVO_2 = 13; // Enable right motor
 
 rpio.open(MOTOR_HL1, rpio.OUTPUT, rpio.LOW);
 rpio.open(MOTOR_HL2, rpio.OUTPUT, rpio.LOW);
@@ -19,8 +19,8 @@ rpio.open(MOTOR_RIGHT_EN, rpio.OUTPUT, rpio.HIGH);
 rpio.open(SERVO_1, rpio.OUTPUT);
 rpio.open(SERVO_2, rpio.OUTPUT);
 
-var servo1 = new PiServo(33); 
-var servo2 = new PiServo(32); 
+var servo1 = new PiServo(12); 
+var servo2 = new PiServo(13); 
 
 class motorControll{
 
