7f8a35e43f11143e342c3ac5b8efb6ca42d657f1
Ivan Pavao Lozancic <ivano.lozani@gmail.com>
2020-01-18 05:59:30 +0100

    AJMOOOOOOOO

--- src/camera/index.js
+++ src/camera/index.js
@@ -21,7 +21,29 @@ const piCamStream = async () => {
     await streamCamera.startCapture();
  
     // We can also listen to data events as they arrive
-    videoStream.on("data", (data) => {console.log("New data", data);  const img = streamCamera.takeImage(); fs.writeFileSync("still-image.jpg", img);});
+    videoStream.on("data", (data) => {
+        console.log("New data", data);  
+        const img = streamCamera.takeImage(); 
+        //fs.writeFileSync("still-image.jpg", img);
+        Jimp.read(img, function(err, image){
+            if (err) {
+                console.error(err);
+                // TODO handle error
+            }
+            var qr = new QrCode();
+            qr.callback = function(err, value) {
+                if (err) {
+                    console.error(err);
+                    // TODO handle error
+                }
+                console.log(value.result);
+                console.log(value);
+            };
+            qr.decode(image.bitmap);
+        })
+    });
+
+    });
     videoStream.on("end", data => console.log("Video stream has ended"));
  
     // Wait for 5 seconds
