9628e0b3e24b0715eb60fe5eed6d1af59ff64d97
jmelvan <jakov.melvan@gmail.com>
2020-01-18 07:53:52 +0100

    asd

--- src/camera/index.js
+++ src/camera/index.js
@@ -8,6 +8,7 @@ raspberryPiCamera.on('frame', (data) => {
 });
 
 fs.watch("test.jpeg", function(curr, prev){
+    console.log(curr);
     qrdecoder.decode("test.jpeg", (err, out) => {
         if(err){
             console.log(err);
