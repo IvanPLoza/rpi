5898a6ee6d487f7644435bbf6d14deda54a14357
jmelvan <jakov.melvan@gmail.com>
2020-01-18 07:23:40 +0100

    asd

--- src/camera/index.js
+++ src/camera/index.js
@@ -5,16 +5,22 @@ var fs = require("fs");
 
 raspberryPiCamera.on('frame', (data) => {
     console.log(data);
-    Jimp.read(data, function(err, img){
-       
-        var qr = new QrCode();
-        qr.callback = function(err, value) {
-            
-            console.log(value.result);
-            console.log(value);
-        };
-        qr.decode(img.bitmap);
-    });;
+    // Jimp.read(data, function(err, img){
+    //     if (err) {
+    //         console.error(err);
+    //         // TODO handle error
+    //     }
+    //     var qr = new QrCode();
+    //     qr.callback = function(err, value) {
+    //         if (err) {
+    //             console.error(err);
+    //             // TODO handle error
+    //         }
+    //         console.log(value.result);
+    //         console.log(value);
+    //     };
+    //     qr.decode(img.bitmap);
+    // });;
 });
 
 raspberryPiCamera.start({
