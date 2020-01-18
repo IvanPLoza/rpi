4604c47caa51342f3a69a6e8b7b7111804174ac5
Ivan Pavao Lozancic <ivano.lozani@gmail.com>
2020-01-18 12:01:58 +0100

    New

--- src/camera/index.js
+++ src/camera/index.js
@@ -1,47 +1,3 @@
-<<<<<<< HEAD
-const { StreamCamera, Codec, SensorMode  } = require( "pi-camera-connect" );
-const fs = require("fs");
-var QrCode = require('qrcode-reader');
-var Jimp = require("jimp");
-//var ImageParser = require("image-parser");
-
-const piCamStream = async () => {
-    
-
-    const streamCamera = new StreamCamera({
-        codec: Codec.H264
-    });
- 
-    const videoStream = streamCamera.createStream();
- 
-    const writeStream = fs.createWriteStream("video-stream.h264");
- 
-    // Pipe the video stream to our video file
-    videoStream.pipe(writeStream);
- 
-    await streamCamera.startCapture();
- 
-    // We can also listen to data events as they arrive
-    videoStream.on("data", (data) => {
-        console.log("New data", data);  
-        const img = streamCamera.takeImage(); 
-        fs.writeFileSync("still-image.jpg", img);
-        
-    });
-    
-    videoStream.on("end", data => console.log("Video stream has ended"));
- 
-    // Wait for 5 seconds
-    await new Promise(resolve => setTimeout(() => resolve(), 5000));
- 
-    await streamCamera.stopCapture();
-    
-};
-
-
-
-piCamStream();
-=======
 const raspberryPiCamera = require('raspberry-pi-camera-native');
 const QRCode = require("qrcode-reader");
 const Jimp = require("jimp");
@@ -69,4 +25,3 @@ raspberryPiCamera.start({
     quality: 50,
     encoding: 'JPEG'
 });
->>>>>>> melvan
