bf1e93efee97de82d44a57c3b0292fe2aaf88efa
Ivan Pavao Lozancic <ivano.lozani@gmail.com>
2020-01-18 05:52:35 +0100

    aisdjasiod

--- src/camera/index.js
+++ src/camera/index.js
@@ -5,16 +5,27 @@ var Jimp = require("jimp");
 //var ImageParser = require("image-parser");
 
 const piCamStream = async () => {
+    
+
     const streamCamera = new StreamCamera({
-        codec: Codec.MJPEG,
-        sensorMode: SensorMode.Mode6
+        codec: Codec.H264
     });
+ 
+    const videoStream = streamCamera.createStream();
+ 
+    const writeStream = fs.createWriteStream("video-stream.h264");
+ 
+    // Pipe the video stream to our video file
+    videoStream.pipe(writeStream);
  
     await streamCamera.startCapture();
  
-    const image = await streamCamera.takeImage();
-    await fs.writeFileSync("image.jpg", image);
-    // Process image...
+    // We can also listen to data events as they arrive
+    videoStream.on("data", (data) => {console.log("New data", data); await const img = streamCamera.takeImage(); fs.writeFileSync("still-image.jpg", img);});
+    videoStream.on("end", data => console.log("Video stream has ended"));
+ 
+    // Wait for 5 seconds
+    await new Promise(resolve => setTimeout(() => resolve(), 5000));
  
     await streamCamera.stopCapture();
     
