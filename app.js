b08fb1826f0325bc31acd13f9f5609b9adacc019
Ivan Pavao Lozancic <ivano.lozani@gmail.com>
2020-01-18 12:00:43 +0100

    merge

--- app.js
+++ app.js
@@ -5,11 +5,5 @@
  * 
  */
 
-<<<<<<< HEAD
-const {motor, testMotors, motorControll} = require("./src/gpioControlls/index.js");
-const {motorEncoder} = require("./src/motorEncoder/index.js");
-require("./src/camera/index.js");
-=======
 const {motor, testServo} = require("./src/gpioControlls/index.js");
 require("./src/camera/index");
->>>>>>> melvan
