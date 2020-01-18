1394e6294b0fc93efdeb0c5bd117b21544c1aaf8
Ivan Pavao Lozancic <ivano.lozani@gmail.com>
2020-01-18 01:00:16 +0100

    Trying new things

--- src/camera/index.js
+++ src/camera/index.js
@@ -2,18 +2,28 @@ const { StreamCamera, Codec } = require( "pi-camera-connect" );
 
 
 const piCamStream = async () => {
-    
+
     const streamCamera = new StreamCamera({
-        codec: Codec.MJPEG
+        codec: Codec.H264
     });
+
+    const videoStream = streamCamera.createStream();
+
+    const writeStream = fs.createWriteStream("video-stream.h264");
+
+    // Pipe the video stream to our video file
+    videoStream.pipe(writeStream);
 
     await streamCamera.startCapture();
 
-    const image = await streamCamera.takeImage();
+    // We can also listen to data events as they arrive
+    videoStream.on("data", data => console.log("New data", data));
+    videoStream.on("end", data => console.log("Video stream has ended"));
+
+    // Wait for 5 seconds
+    await new Promise(resolve => setTimeout(() => resolve(), 5000));
 
     await streamCamera.stopCapture();
-
-    console.log("IMAGE IS: " + image);
 };
 
 
