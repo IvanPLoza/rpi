217b6fba7a2ff9d6c56d1413cbca4b50f77bce1e
jmelvan <jakov.melvan@gmail.com>
2020-01-18 08:01:03 +0100

    asd

--- app.js
+++ app.js
@@ -5,5 +5,5 @@
  * 
  */
 
-const {motor, testServo} = require("./src/gpioControlls/index.js");
+//const {motor, testServo} = require("./src/gpioControlls/index.js");
 require("./src/camera/index");
