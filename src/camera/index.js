e62b57e7d5063ce491445bf47f783f2b86166415
jmelvan <jakov.melvan@gmail.com>
2020-01-18 07:13:01 +0100

    asd

--- src/camera/index.js
+++ src/camera/index.js
@@ -2,15 +2,26 @@ const raspberryPiCamera = require('raspberry-pi-camera-native');
 var QrCode = require("qrcode-reader");
 var Jimp = require("jimp");
 
-var buffers = [];
+var buffers = Jimp.read(buffers, function(err, img){
+    if (err) {
+        console.error(err);
+        // TODO handle error
+    }
+    var qr = new QrCode();
+    qr.callback = function(err, value) {
+        if (err) {
+            console.error(err);
+            // TODO handle error
+        }
+        console.log(value.result);
+        console.log(value);
+    };
+    qr.decode(img.bitmap);
+});;
 
 raspberryPiCamera.on('frame', (data) => {
-    //console.log(data);
+    console.log(data);
     data.pipe(buffers);
-});
-
-buffers.on('change', function(err, val) {
-    console.log("here");
 });
 
 raspberryPiCamera.start({
